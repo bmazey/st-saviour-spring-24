63761119ac4bbcb9d42d2264f37af66cb0a592c9
brandon mazey <brandon@brandons-Air.localdomain>
2024-01-31 11:03:57 -0500

    example

--- src/main/java/Topics.java
+++ src/main/java/Topics.java
@@ -4,7 +4,7 @@ public class Topics {
     public static void main(String[] args) {
 
         // Create a regular expression.
-        Pattern pattern = Pattern.compile("abc.*");
+        Pattern pattern = Pattern.compile("abc.");
 
         // Check to see if given text is matched by the regex pattern.
         System.out.println(pattern.matcher("abcd").matches());
