b09180ad30c24a24d1569a6d7b5b93106a5bf376
slandai2 <samara.landais@gmail.com>
2024-02-07 13:12:10 -0500

    completed up to vertical

--- src/main/java/TurkishRug.java
+++ src/main/java/TurkishRug.java
@@ -8,26 +8,66 @@ public class TurkishRug {
 
     // Creates a solid 2D String array.
     public static String[][] solid() {
-        // TODO implement solid()
-        return new String[0][0];
+        String emoji = new String(Character.toChars(0x2665));
+        String [][] solid = new String[11][11];
+        for (int i = 0; i < solid.length; i++){
+            for (int j = 0; j < solid.length; j++){
+                solid[i][j] = emoji;
+            }
+        }
+        return solid; 
     }
     
     // Creates a horizontal striped 2D String array. 
     public static String[][] horizontal() {
-        // TODO implement horizontal()
-        return new String[0][0];
+        String club = new String(Character.toChars(0x2663));
+        String spade = new String (Character.toChars(0x2660));
+        String [][] horizontal = new String [11][11];
+        for (int i = 0; i < horizontal.length; i++){
+            for (int j = 0; j < horizontal.length; j++){
+                if (i % 2== 0){
+                    horizontal[i][j] = club;
+                } else{
+                    horizontal [i][j] = spade;
+                }
+            }
+        }
+        return horizontal;
     }
 
     // Creates a vertical striped 2D String array.
     public static String[][] vertical() {
-        // TODO implement vertical()
-        return new String[0][0];
+        String [][] vertical = new String [11][11];
+        String club = new String (Character.toChars(0x2663));
+        String spade = new String (Character.toChars(0x2660));
+        for (int i = 0; i < vertical.length; i++){
+            for (int j = 0; j < vertical.length; j++){
+                if (j % 2 ==0){
+                    vertical[i][j] = club;
+                }else{
+                    vertical[i][j] = spade;
+                }
+            }
+        }
+        return vertical;
     }
 
     // Creates a diagonally striped 2D String array.
     public static String[][] diagonal() {
         // TODO implement diagonal()
-        return new String[0][0];
+        String [][] diagonal = new String [11][11];
+        String dragon = new String (Character.toChars(0x1F432));
+        String rabbit = new String (Character.toChars(0x1F430));
+        for (int i = 0; i < diagonal.length; i ++){
+            for (int j = 0; j < diagonal.length; j++){
+                if (i = 0){ // work on this 
+                    diagonal[i][j] = dragon; 
+                }else{
+                    diagonal[i][j] = rabbit;
+                }
+            }
+        }
+        return diagonal; 
     }
 
     // Creates a plaid 2D String array.
