506533e9efc5b27e3b386e5e3238b1ca6f50bb5d
AFWood <audreyfwood@gmail.com>
2024-02-08 09:38:51 -0500

    updates to solid and horizontal rugs

--- src/main/java/TurkishRug.java
+++ src/main/java/TurkishRug.java
@@ -6,16 +6,41 @@ public class TurkishRug {
     // Emoji unicode reference:
     // https://apps.timwhitlock.info/emoji/tables/unicode
 
+    // emojis to use:
+    // pink flower: U+1F338
+    // yellow flower: U+1F33C
+    // moon: U+1F31A
+    // sun: U+1F31E
+
+
     // Creates a solid 2D String array.
     public static String[][] solid() {
-        // TODO implement solid()
-        return new String[0][0];
+        
+        String sunemoji = new String(Character.toChars(0x1F31E));
+        String[][] solidRug = new String[11][11];
+        for (int i = 0; i < solidRug.length; i++){
+            for (int j = 0; j < solidRug[i].length; j++){
+                solidRug[i][j] = sunemoji;
+            }
+        }
+        return solidRug;
     }
     
     // Creates a horizontal striped 2D String array. 
     public static String[][] horizontal() {
-        // TODO implement horizontal()
-        return new String[0][0];
+        String sunemoji = new String(Character.toChars(0x1F31E));
+        String moonemoji = new String(Character.toChars(0x1F31A));
+        String[][] horizontalRug = new String [11][11];
+        for (int i = 0; i< horizontalRug.length; i++){
+            for (int j = 0; j< horizontalRug[i].length; j++){
+                if (i%2 == 0){
+                    horizontalRug[i][j] = sunemoji;
+                } else {
+                    horizontalRug[i][j] = moonemoji;
+                }
+            }
+        }
+        return horizontalRug;
     }
 
     // Creates a vertical striped 2D String array.
