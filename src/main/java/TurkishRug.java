4dd1b5f07579720f02423e600de53bf5a959b7eb
aladinjng <jossnayah@gmail.com>
2024-02-16 17:21:45 -0500

    done

--- src/main/java/TurkishRug.java
+++ src/main/java/TurkishRug.java
@@ -8,11 +8,14 @@ public class TurkishRug {
 
     // Creates a solid 2D String array.
     public static String[][] solid() {
-        String appleEmoji = new String(Character.toChars(0x1F34F));
+        // Sets the value of an emoji
+        String dangoEmoji = new String(Character.toChars(0x1F361));
+        // Sets a 2D String array for the solid rug
         String[][] solidRug = new String[11][11];
+        // Fill the array with dango emojis
         for (int i = 0; i < solidRug.length; i++){
             for (int j = 0; j < solidRug[i].length; j++){
-                solidRug[i][j] = appleEmoji;
+                solidRug[i][j] = dangoEmoji;
             }
         }
         return solidRug;
@@ -20,13 +23,14 @@ public class TurkishRug {
     
     // Creates a horizontal striped 2D String array. 
     public static String[][] horizontal() {
-        String appleEmoji = new String(Character.toChars(0x1F34F));
+        String dangoEmoji = new String(Character.toChars(0x1F361));
         String swirlEmoji = new String(Character.toChars(0x1F365));
         String[][] horizontalRug = new String[11][11];
         for (int i = 0; i < horizontalRug.length; i++){
             for (int j = 0; j < horizontalRug[i].length; j++){
+                // When i is even, the array has dango emojis; otherwise, swirl emojis
                 if (i % 2 == 0) {
-                    horizontalRug[i][j] = appleEmoji;
+                    horizontalRug[i][j] = dangoEmoji;
                 } else {
                     horizontalRug[i][j] = swirlEmoji;
                 }
@@ -37,13 +41,14 @@ public class TurkishRug {
 
     // Creates a vertical striped 2D String array.
     public static String[][] vertical() {
-        String appleEmoji = new String(Character.toChars(0x1F34F));
+        String dangoEmoji = new String(Character.toChars(0x1F361));
         String swirlEmoji = new String(Character.toChars(0x1F365));
         String[][] verticalRug = new String[11][11];
         for (int i = 0; i < verticalRug.length; i++){
             for (int j = 0; j < verticalRug[i].length; j++){
+                // When j is even, the array has dango emojis; otherwise, swirl emojis.
                 if (j % 2 == 0) {
-                    verticalRug[i][j] = appleEmoji;
+                    verticalRug[i][j] = dangoEmoji;
                 } else {
                     verticalRug[i][j] = swirlEmoji;
                 }
@@ -54,17 +59,19 @@ public class TurkishRug {
 
     // Creates a diagonally striped 2D String array.
     public static String[][] diagonal() {
-        String appleEmoji = new String(Character.toChars(0x1F34F));
+        String dangoEmoji = new String(Character.toChars(0x1F361));
         String swirlEmoji = new String(Character.toChars(0x1F365));
         String[][] diagonalRug = new String[11][11];
         for (int i = 0; i < diagonalRug.length; i++) {
             for (int j = 0; j < diagonalRug[i].length; j++) {
                 diagonalRug[i][j] = swirlEmoji;
+                // When both i and j are even, the array has dango emojis
                 if (i % 2 == 0 && j % 2 == 0) {
-                    diagonalRug[i][j] = appleEmoji;
+                    diagonalRug[i][j] = dangoEmoji;
                 }
+                // When both i and j are odd, the array has swirl emojis
                 if (i % 2 != 0 && j % 2 != 0) {
-                    diagonalRug[i][j] = appleEmoji;
+                    diagonalRug[i][j] = dangoEmoji;
                 }
             }
         }
@@ -73,14 +80,16 @@ public class TurkishRug {
 
     // Creates a plaid 2D String array.
     public static String[][] plaid() {
-        String appleEmoji = new String(Character.toChars(0x1F34F));
+        String dangoEmoji = new String(Character.toChars(0x1F361));
         String swirlEmoji = new String(Character.toChars(0x1F365));
         String[][] plaidRug = new String[11][11];
         for (int i = 0; i < plaidRug.length; i++) {
             for (int j = 0; j < plaidRug[i].length; j++) {
+                // Sets swirl emoji as default in the array
                 plaidRug[i][j] = swirlEmoji;
+                // When both i and j are even, the array has dango emojis
                 if (i % 2 == 0 && j % 2 == 0) {
-                    plaidRug[i][j] = appleEmoji;
+                    plaidRug[i][j] = dangoEmoji;
                 }
             }
         }
@@ -89,23 +98,24 @@ public class TurkishRug {
 
     // BONUS: Creates an argyle 2D String array.
     public static String[][] argyle() {
-        String appleEmoji = new String(Character.toChars(0x1F34F));
+        String dangoEmoji = new String(Character.toChars(0x1F361));
         String swirlEmoji = new String(Character.toChars(0x1F365));
         String[][] argyleRug = new String[11][11];
         for (int i = 0; i < argyleRug.length; i++) {
             for (int j = 0; j < argyleRug[i].length; j++) {
+                // States the conditions for placing dango emojis to create argyle pattern
                 if (i == j) {
-                    argyleRug[i][j] = appleEmoji;
+                    argyleRug[i][j] = dangoEmoji;
                 } else if (i + j == 10) {
-                    argyleRug[i][j] = appleEmoji;
+                    argyleRug[i][j] = dangoEmoji;
                 } else if (i + j == 5) {
-                    argyleRug[i][j] = appleEmoji;
+                    argyleRug[i][j] = dangoEmoji;
                 } else if (j - i == 5) {
-                    argyleRug[i][j] = appleEmoji;
+                    argyleRug[i][j] = dangoEmoji;
                 } else if (i - j == 5) {
-                    argyleRug[i][j] = appleEmoji;
+                    argyleRug[i][j] = dangoEmoji;
                 } else if (i + j == 15) {
-                    argyleRug[i][j] = appleEmoji;
+                    argyleRug[i][j] = dangoEmoji;
                 } else {
                     argyleRug[i][j] = swirlEmoji;
                 }
