1b29d95f7306671517085d051a9e22bf8e1203a9
CCCCadet <cassiewcadet@gmail.com>
2024-02-16 09:37:12 -0500

    lab completed

--- src/main/java/TurkishRug.java
+++ src/main/java/TurkishRug.java
@@ -9,31 +9,87 @@ public class TurkishRug {
     // Creates a solid 2D String array.
     public static String[][] solid() {
         // TODO implement solid()
-        return new String[0][0];
+        String bowemoji = new String(Character. toChars (0x1F380));
+        String [][] solidrug = new String [11][11];
+        for (int i = 0; i < solidrug.length; i++){
+            for (int j = 0; j < solidrug. length; j++){
+                solidrug[i][j] = bowemoji;
+            }
+        }
+        return solidrug;
     }
-    
+
     // Creates a horizontal striped 2D String array. 
     public static String[][] horizontal() {
         // TODO implement horizontal()
-        return new String[0][0];
+        String bowemoji = new String(Character.toChars(0x1F380));
+        String diamondemoji = new String(Character.toChars(0x1F48E));
+        String [][] horizontalRug = new String [11][11];
+        for (int i = 0; i < horizontalRug.length; i++) {
+            for (int j = 0; j < horizontalRug[i].length; j++){
+                if (i%2 == 0){
+                    horizontalRug[i][j] = bowemoji;
+                } else{
+                    horizontalRug [i][j] = diamondemoji;
+                }
+            }
+        }
+        return horizontalRug;
     }
+    
 
     // Creates a vertical striped 2D String array.
     public static String[][] vertical() {
-        // TODO implement vertical()
-        return new String[0][0];
+        String bowemoji = new String(Character.toChars(0x1F380));
+        String diamondemoji = new String(Character.toChars(0x1F48E));
+        String[][] verticalRug = new String[11][11];
+        for (int i = 0; i < verticalRug.length; i++) {
+            for (int j = 0; j < verticalRug[i].length; j++){
+                if (j%2 == 0){
+                    verticalRug[i][j] = bowemoji;
+                }else{
+                    verticalRug[i][j] = diamondemoji;
+                }
+            }
+        }
+        return verticalRug;
     }
 
     // Creates a diagonally striped 2D String array.
     public static String[][] diagonal() {
         // TODO implement diagonal()
-        return new String[0][0];
+        String bowemoji = new String(Character.toChars(0x1F380));
+        String diamondemoji = new String(Character.toChars(0x1F48E));
+        String[][] diagonalRug = new String[11][11];
+        for (int i = 0; i < diagonalRug.length; i++) {
+            for (int j = 0; j < diagonalRug[i].length; j++){
+                if (j%2 == 0 ^ i%2 == 0){
+                    diagonalRug[i][j] = bowemoji;
+                } else {
+                    diagonalRug[i][j] = diamondemoji;
+                } 
+            }
+        } 
+        return diagonalRug;  
     }
 
     // Creates a plaid 2D String array.
     public static String[][] plaid() {
         // TODO implement plaid()
-        return new String[0][0];
+        String bowemoji = new String(Character.toChars(0x1F380));
+        String diamondemoji = new String(Character.toChars(0x1F48E));
+        String[][] plaidRug = new String[11][11];
+        for (int i = 0; i < plaidRug.length; i++){
+            for ( int j = 0; j < plaidRug.length; j++){
+                if (j%2 == 0 && i%2 == 0){
+                    plaidRug[i][j] = bowemoji;
+                } else {
+                    plaidRug[i][j] = diamondemoji;
+                }
+
+            }
+        }
+        return plaidRug;
     }
 
     // BONUS: Creates an argyle 2D String array.
