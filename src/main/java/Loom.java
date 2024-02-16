2961bc176c4a9e0d9cb62ac57d5c604d6178f7ba
AFWood <audreyfwood@gmail.com>
2024-02-16 10:25:49 -0500

    final commit

--- src/main/java/Loom.java
+++ src/main/java/Loom.java
@@ -1,5 +1,5 @@
 public class Loom {
-
+    // creates a method that will print any string array
     public static void main(String[] args) {
         weave(TurkishRug.solid());
         weave(TurkishRug.horizontal());
@@ -10,12 +10,17 @@ public class Loom {
     }
     // Prints a 2D String array.
     public static void weave(String[][] rug) {
-        for (int i = 0; i < rug.length; i++){
-            for (int j = 0; j < rug[i].length; j++){
+        // iterates through each row of the rug
+        for (int i = 0; i < rug.length; i++) {
+            // iterates through each element in each row of the rug
+            for (int j = 0; j < rug[i].length; j++) {
+                // prints each element in each row
                 System.out.print(rug[i][j]);
             }
+            // line break between each row
             System.out.println("");
         }
+        // line break between each rug
         System.out.println("");
     }   
 }
