d82f33d5ab6fed8001e2e1492cb08408805d49fa
giologym <giodmil@gmail.com>
2024-03-15 09:29:48 -0400

    Detective

--- src/main/java/Detector.java
+++ src/main/java/Detector.java
@@ -18,8 +18,14 @@ public class Detector {
     public Detector(String pattern, int q) {
         // TODO - implement constructor
         // 1. populate pattern and q
+        this.pattern = pattern; 
+        this.q = q;
+
         // 2. compute h
+        this.computeH();
+
         // 3. compute the pattern hash
+        this.computePHash();
     }
 
     private void computeH() {
@@ -27,7 +33,10 @@ public class Detector {
         // 1. loop over the pattern
         // 2. for each character in the pattern ...
         //    - multiply h by the size of character set 
-        //    - hash the result set it equal to the current value of h 
+        //    - hash the result, set it equal to the current value of h 
+        for(int i =0; i < pattern.length() - 1; i++) {
+            h = (h*d) % q;
+        }
     }
 
     private void computePHash() {
@@ -37,6 +46,9 @@ public class Detector {
         //    - multiply the size of the character set by the current pattern hash
         //    - add the character at the current position
         //    - hash the entire result and set it equal to the current value of pattern hash
+        for(int i = 0; i < pattern.length(); i++) {
+            phash = (d * phash + pattern.charAt(i)) % q;
+        }
     }
 
     // search returns the position of the first occurence of plagiarism, or a -1 if none detected.
@@ -52,20 +64,35 @@ public class Detector {
         //    - multiply the text hash by the size of the character set
         //    - add the value of the current *text hash* character (not pattern!)
         //    - hash the result and update thash accordingly 
-
-        // slide the pattern over the text one by one
+        // 3. slide the pattern over the text one by one
         // make sure to correctly calculate the terminating condition of the loop!
         // (HINT: length of text - length of pattern)
         // for each position ...
-        // 1. check if the pattern hash equals the text hash
-        //    - if it does, we need to rule out a possible hash collision.
-        //    - compare the text window string to the pattern string, return the position if equal (plagiarism match)
-        //    - if they are NOT equal, continue
-        // 2. check if the current position is is less than the text window length - pattern length.
-        //    - if so, compute the next value for text hash by removing the leading digit and adding the trailing digit.
-        //    - ex: thash = (d * (thash - text.charAt(i) * h) + text.charAt(i + pattern.length())) % q;
-        //    - if text hash is negative, flip to positive by adding the value of the prime to current text hash.
-        // 3. if there are no matches, return -1
+        for(int i = 0; i < pattern.length(); i++) {
+            thash = (thash * d + text.charAt(i)) % q;
+        }
+
+        for(int i = 0; i <= text.length()-pattern.length(); i++) {
+            if (phash == thash) {
+                System.out.println("collision detected");
+                for(int j=0; j< pattern.length(); j++) {
+                    if(text.charAt(i+j) != pattern.charAt(j)) {
+                        break;
+                    }
+                    if(j == pattern.length() - 1) {
+                        return i;
+                    }
+                }
+            } 
+            if (i < text.length()- pattern.length()){
+                thash = (d * (thash - text.charAt(i) * h) + text.charAt(i + pattern.length())) % q;
+                if (thash < 0) {
+                    thash = thash + q;
+                }
+            }
+        }   
+        
         return -1;
+
     }
 }
