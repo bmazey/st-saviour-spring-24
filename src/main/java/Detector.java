4aec634e20d8b9ee1ec391e51b28dbc273544e9c
CCCCadet <cassiewcadet@gmail.com>
2024-03-15 08:43:54 -0400

    cassie plagiarism

--- src/main/java/Detector.java
+++ src/main/java/Detector.java
@@ -17,55 +17,57 @@ public class Detector {
 
     public Detector(String pattern, int q) {
         // TODO - implement constructor
-        // 1. populate pattern and q
-        // 2. compute h
+        this.q = q; 
+         // 1. populate pattern and q
+        this.pattern = pattern;
         // 3. compute the pattern hash
+        this.computeH();
+        this.computePHash();
+        
     }
 
     private void computeH() {
         // TODO - implement conputeH()
-        // 1. loop over the pattern
-        // 2. for each character in the pattern ...
-        //    - multiply h by the size of character set 
-        //    - hash the result set it equal to the current value of h 
+        // loop over the pattern
+        for (int i = 0; i < pattern.length() -1; i++){
+            h = (h * d) % q;
+        }
     }
-
+        
     private void computePHash() {
         // TODO - implement computePHash()
         // 1. loop over the pattern
-        // 2. for each character in the pattern ...
-        //    - multiply the size of the character set by the current pattern hash
-        //    - add the character at the current position
-        //    - hash the entire result and set it equal to the current value of pattern hash
+           for (int i = 0; i < pattern.length(); i++){
+            phash = ((phash * d) + pattern.charAt(i)) % q;
+        }
+
     }
 
-    // search returns the position of the first occurence of plagiarism, or a -1 if none detected.
+        
     public int search(String text) {
-        // TODO implement search()
-        // tash represents the hash of the current text window
-        // set thash to 0
         int thash = 0;
-
-        // start by computing the hash value of the first window of text
-        // 1. loop over the pattern
-        // 2. for each character ...
-        //    - multiply the text hash by the size of the character set
-        //    - add the value of the current *text hash* character (not pattern!)
-        //    - hash the result and update thash accordingly 
-
-        // slide the pattern over the text one by one
-        // make sure to correctly calculate the terminating condition of the loop!
-        // (HINT: length of text - length of pattern)
-        // for each position ...
-        // 1. check if the pattern hash equals the text hash
-        //    - if it does, we need to rule out a possible hash collision.
-        //    - compare the text window string to the pattern string, return the position if equal (plagiarism match)
-        //    - if they are NOT equal, continue
-        // 2. check if the current position is is less than the text window length - pattern length.
-        //    - if so, compute the next value for text hash by removing the leading digit and adding the trailing digit.
-        //    - ex: thash = (d * (thash - text.charAt(i) * h) + text.charAt(i + pattern.length())) % q;
-        //    - if text hash is negative, flip to positive by adding the value of the prime to current text hash.
-        // 3. if there are no matches, return -1
+     for (int i = 0; i < pattern.length(); i++){
+            thash = ((thash * d) + text.charAt(i)) % q;
+        }
+        for (int i = 0; i <= text.length() - pattern.length(); i++){
+            if (phash == thash){
+            for (int j = 0; j < pattern.length(); j++){
+                    if (text.charAt(i + j) != pattern.charAt(j)){
+                        break;
+                    }
+                    if (j == pattern.length()-1){
+                        return i;
+                    }
+                }
+            } 
+            if ( i < text.length() - pattern.length()){
+                thash = (d * (thash - text.charAt(i) * h) + text.charAt(i + pattern.length())) % q;
+                if (thash < 0){
+                    thash = thash + q;
+                }
+            }
+            }
+       
         return -1;
     }
 }
