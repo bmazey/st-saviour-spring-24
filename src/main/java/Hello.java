af2c577aaacd76f9bc7d05dd1e9c48eabfc496fe
brandon mazey <brandon@brandons-Air.localdomain>
2024-04-09 10:47:24 -0400

    singleton

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,12 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("new dawn, new day");
+
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println("singleton1: " + singleton1.toString());
+        System.out.println("singleton2: " + singleton2.toString());
     }
     
 }
