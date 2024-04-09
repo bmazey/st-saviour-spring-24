8b007e304b1821a1c32417ae697b1e0ece496a70
Reigndrop334 <rainydrops33@gmail.com>
2024-04-09 10:52:13 -0400

    :wq

---
+++ src/main/java/Singleton.java
@@ -0,0 +1,23 @@
+public class  Singleton {
+    private static Singleton INSTANCE;
+
+    private String info = "original info";
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Singleton();
+        }
+        return INSTANCE;
+    }
+    public String getInfo() {
+        return this.info;
+}
+
+    public void setInfo(String info) {
+        this.info = info;
+}
+
+}
+
