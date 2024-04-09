7ea8b16d63b35e4d6e607bf33f7e52acf6a55907
AMVenetian <antonellavenetian@gmail.com>
2024-04-09 10:49:11 -0400

    Singleton implementation done

---
+++ src/main/java/test/java/SingletonTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test:
+
+public class SingletonTest {
+    @Test 
+    public void TestSingleton() {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        assertEquals(singleton1, singleton2);
+
+        assert singleton1 == singleton2;
+
+        singleton1.setInfo("new info just dropped!");
+        assert singleton2.getInfo().equals("new info just dropped!");
+    }
+}
