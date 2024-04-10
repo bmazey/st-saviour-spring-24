9ed6b289a234acd38697c8432da9f0514b0694f4
Reigndrop334 <rainydrops33@gmail.com>
2024-04-10 13:54:38 -0400

    startng builder

---
+++ src/main/java/DatingBuilder.java
@@ -0,0 +1,14 @@
+public static class DatingBuilder{
+    private String name; 
+    private int age;
+    private String hobbies;
+    private String location;
+
+    public static class DatingBuilder (String name, String age) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    
+}
