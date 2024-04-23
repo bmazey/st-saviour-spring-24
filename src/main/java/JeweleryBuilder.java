ae8d2279941694b75ec479ea02929bc4e9625822
aladinjng <jossnayah@gmail.com>
2024-04-23 08:34:17 -0400

    update

--- src/main/java/JeweleryBuilder.java
+++ src/main/java/JeweleryBuilder.java
@@ -4,13 +4,13 @@ public class JeweleryBuilder {
     private String silver;
     private String gemstones;
     
-    public JeweleryBuilder metalType(String gold, String silver) {
+    public JeweleryBuilder setMetalType(String gold, String silver) {
         this.gold = gold;
         this.silver = silver;
         return this;
     }
 
-    public JeweleryBuilder gemstoneType(String gemstones) {
+    public JeweleryBuilder SetGemstoneType(String gemstones) {
         this.gemstones = gemstones;
         return this;
     }
