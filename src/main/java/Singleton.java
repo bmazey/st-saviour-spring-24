74bf07c91497a7dbdd421ab641d4b922650c2333
Giovanna Milisic <giodmil@gmail.com>
2024-04-25 11:01:37 -0400

    jewels

---
+++ src/main/java/Singleton.java
@@ -0,0 +1,22 @@
+public class Singleton {
+    private static Singleton INSTANCE;
+
+    private String info = "original info";
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Singleton();
+        }
+        return INSTANCE;
+    }
+
+    public String getInfo() {
+        return this.info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
