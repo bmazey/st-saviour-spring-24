d5d636e32d94f11b329a16078ee638402cc8dcf2
AMVenetian <antonellavenetian@gmail.com>
2024-04-25 12:00:24 -0400

    creating builder test

--- src/main/java/Outfit.java
+++ src/main/java/Outfit.java
@@ -1,40 +1,40 @@
 public class Outfit {
-    
-    private String shirt;
-    private String pants;
-    private String accesories;
-    private boolean hat;
-    
-    public String setShirt(){
-        return shirt;
-    }
-    public void getShirt(String shirt){
-        this.shirt = shirt;
-    }
-    
-    public String setPants(){
-        return pants;
-    }
-    public void getPants(String pants){
-        this.pants = pants;
-    }
-    
-    public String setAccesories(){
-        return accesories;
-    }
-    public void getAccesories(String accesories){
-        this.accesories = accesories;
-    }
-
-    public boolean setHat(){
-        return hat;
-    }
-    public void getHat(boolean hat){
-        this.hat = hat;
-    }
 
     public static class OutfitBuilder {
         
+        private String shirt;
+        private String pants;
+        private String accesories;
+        private boolean hat;
+    
+        public String setShirt(){
+            return shirt;
+        }
+        public void getShirt(String shirt){
+            this.shirt = shirt;
+        }  
+    
+        public String setPants(){
+            return pants;
+        }
+        public void getPants(String pants){
+            this.pants = pants;
+        }
+    
+        public String setAccesories(){
+            return accesories;
+        }
+        public void getAccesories(String accesories){
+            this.accesories = accesories;
+        }
+
+        public boolean setHat(){
+            return hat;
+        }
+        public void getHat(boolean hat){
+            this.hat = hat;
+        }
+
         public OutfitBuilder(String shirt, String pants) {
             this.shirt = shirt;
             this.pants = pants;
@@ -51,7 +51,7 @@ public class Outfit {
         }
         
         public Outfit build() {
-            return new Outfit(this);
+            return new Outfit();
         }
     }
     
---
+++ src/test/java/OutfitBuilderTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.DynamicTest.stream;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+public class OutfitBuilderTest {
+    
+}
