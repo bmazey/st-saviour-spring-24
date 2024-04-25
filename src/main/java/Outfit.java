c9a1384c2c034034d4bfc4a78a7d6fb1d9e5fbbb
AMVenetian <antonellavenetian@gmail.com>
2024-04-25 11:08:38 -0400

    creating builder pattern

---
+++ src/main/java/Outfit.java
@@ -0,0 +1,58 @@
+public class Outfit {
+    
+    private String shirt;
+    private String pants;
+    private String accesories;
+    private boolean hat;
+    
+    public String setShirt(){
+        return shirt;
+    }
+    public void getShirt(String shirt){
+        this.shirt = shirt;
+    }
+    
+    public String setPants(){
+        return pants;
+    }
+    public void getPants(String pants){
+        this.pants = pants;
+    }
+    
+    public String setAccesories(){
+        return accesories;
+    }
+    public void getAccesories(String accesories){
+        this.accesories = accesories;
+    }
+
+    public boolean setHat(){
+        return hat;
+    }
+    public void getHat(boolean hat){
+        this.hat = hat;
+    }
+
+    public static class OutfitBuilder {
+        
+        public OutfitBuilder(String shirt, String pants) {
+            this.shirt = shirt;
+            this.pants = pants;
+        }
+    
+        public OutfitBuilder withAccesories(String accesories) {
+            this.accesories = accesories;
+            return this;
+        }
+    
+        public OutfitBuilder wantsHat(boolean hat) {
+            this.hat = hat;
+            return this;
+        }
+        
+        public Outfit build() {
+            return new Outfit(this);
+        }
+    }
+    
+}
