b2304ef2c97e954127a9fdd630b78e9c9ada2552
AMVenetian <antonellavenetian@gmail.com>
2024-04-27 17:02:00 -0400

    completed outfit builder and test

--- src/main/java/Outfit.java
+++ src/main/java/Outfit.java
@@ -1,58 +1,73 @@
 public class Outfit {
 
-    public static class OutfitBuilder {
-        
+    //creates fields that represent outfit pieces
+    private String shirt;
+
+    private String pants;
+
+    private String shoes;
+
+    //assert that builder is a part of the outfit method, and class
+    Outfit(Builder builder) {
+        this.shirt = builder.shirt;
+        this.pants = builder.pants;
+        this.shoes = builder.shoes;
+    }
+
+    Outfit() {}
+
+    //setters and getters for all the outfit components
+    public String getShirt() {
+        return shirt;
+    }
+
+    public String getPants() {
+        return pants;
+    }
+
+    public String getShoes() {
+        return shoes;
+    }
+
+    public void setShirt(String shirt) {
+        this.shirt = shirt;
+    }
+
+    public void setPants(String pants) {
+        this.pants = pants;
+    }
+
+    public void setShoes(String shoes) {
+        this.shoes = shoes;
+    }
+
+    //creates a static class within Outfit that puts together the components of an outfit
+    public static class Builder {
         private String shirt;
         private String pants;
-        private String accesories;
-        private boolean hat;
-    
-        public String setShirt(){
-            return shirt;
-        }
-        public void getShirt(String shirt){
-            this.shirt = shirt;
-        }  
-    
-        public String setPants(){
-            return pants;
-        }
-        public void getPants(String pants){
-            this.pants = pants;
-        }
-    
-        public String setAccesories(){
-            return accesories;
-        }
-        public void getAccesories(String accesories){
-            this.accesories = accesories;
-        }
+        private String shoes;
 
-        public boolean setHat(){
-            return hat;
-        }
-        public void getHat(boolean hat){
-            this.hat = hat;
-        }
+        public Builder() {}
 
-        public OutfitBuilder(String shirt, String pants) {
+        //returns the accesories using keyword this
+        public Builder Shirt(String shirt) {
             this.shirt = shirt;
-            this.pants = pants;
-        }
-    
-        public OutfitBuilder withAccesories(String accesories) {
-            this.accesories = accesories;
             return this;
         }
-    
-        public OutfitBuilder wantsHat(boolean hat) {
-            this.hat = hat;
+
+        public Builder Pants(String pants) {
+            this.pants = pants;
             return this;
         }
-        
+
+        public Builder Shoes(String shoes) {
+            this.shoes = shoes;
+            return this;
+        }
+
+        //builds the entire outfit in a method 'build'
         public Outfit build() {
-            return new Outfit();
+            return new Outfit(this);
         }
     }
-    
 }
