51add995c6e59bf39568e490a7817bea45e06e8a
egalenyc <lexi.degale@gmail.com>
2024-04-28 22:16:57 -0400

    update

---
+++ src/main/java/Jewelry.java
@@ -0,0 +1,50 @@
+public class Jewelry {
+
+    private String metal;
+    private String gemstones;
+    private String jewelryCategory;
+
+    public Jewelry(JewelryBuilder builder) {
+        this.metal = builder.metal;
+        this.gemstones = builder.gemstones;
+        this.jewelryCategory = builder.jewelryCategory;
+    }
+
+    public String getMetal() {
+        return this.metal;
+    }
+
+    public String getGemstones() {
+        return this.gemstones;
+    }
+
+    public String getCategory(){
+        return this.jewelryCategory;
+    }
+
+    public static class JewelryBuilder {
+
+        private String metal;
+        private String gemstones;
+        private String jewelryCategory;
+
+        public JewelryBuilder metalType(String type) {
+            this.metal = type;
+            return this;
+        }
+
+        public JewelryBuilder withGems (String gemstones) {
+            this. gemstones = gemstones;
+            return this;
+        }
+
+        public JewelryBuilder whatCategory (String jewelryCategory) {
+            this.jewelryCategory = jewelryCategory;
+            return this;
+        }
+
+        public Jewelry build() {
+            return new Jewelry(this);
+        }
+    }
+}
